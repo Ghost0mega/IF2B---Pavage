b702cbfe18e9c44695d86bfaff07683872b5757b
Ghost0mega <sguyenot009@gmail.com>
2024-05-21 14:47:47 +0200

    Utilities Edits
    - Removed fillInMatrix and matricesMultiplication as they are useless
    - edited the remaining functions to work with the char type

--- Stuff/Utilities.h
+++ Stuff/Utilities.h
@@ -14,27 +14,7 @@
  * @param columnNumber - number of columns
  * @return the newly initialized matrix
  */
-float** createAndInitializeMatrix (int lineNumber, int columnNumber);
-
-/**
- * Allow the user to fill in the content of the specified matrix.
- * @param lineNumber - number of lines
- * @param columnNumber - number of columns
- * @param a matrix [lineNumber X columnNumber] filled from user inputs, it will be modified by this function (by address parameter passing)
- */
-void fillInMatrix (int lineNumber, int columnNumber, float*** matrix);
-
-/**
- * Compute the multiplication between two matrices A and B
- * @param lineNumberA - number of lines of the first matrix
- * @param columnNumberA - number of columns of the first matrix
- * @param matrixA - the matrix [lineNumberA X columnNumberA] as first operand of the multiplication
- * @param lineNumberB - number of lines of the second matrix
- * @param columnNumberB - number of columns of the second matrix
- * @param matrixB - the matrix [lineNumberB X columnNumberB] as second operand of the multiplication
- * @return a matrix correspodnign to the multiplication of matrixA x matrixB
- */
-float** matricesMultiplication (int lineNumberA, int columnNumberA, float** matrixA, int lineNumberB, int columnNumberB, float** matrixB);
+char** createAndInitializeMatrix (int lineNumber, int columnNumber);
 
 /**
  * Print the content of the specified matrix
@@ -42,13 +22,13 @@ float** matricesMultiplication (int lineNumberA, int columnNumberA, float** matr
  * @paramcolumnNumber - number of columns
  * @param matrix - the matrix [lineNumber X columnNumber] to print
  */
-void printMatrix (int lineNumber, int columnNumber, float** matrix);
+void printMatrix (int lineNumber, int columnNumber, char** matrix);
 
 /**
  * Free the the content of the specified matrix
  * @paramlineNumber - number of lines
  * @param matrix - the matrix to free, it will be modified by this function (by address parameter passing)
  */
-void freeMatrix (int lineNumber, float*** matrix);
+void freeMatrix (int lineNumber, char*** matrix);
 
 #endif //STUFF_UTILITIES_H
